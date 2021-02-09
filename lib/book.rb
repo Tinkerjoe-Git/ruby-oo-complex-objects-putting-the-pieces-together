3f4fb8b68f67284b060eade5bc816abfd04838c8
Tinkergit <nicholas.stagno@gmail.com>
2021-02-09 13:41:49 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
+class Book
+    @@pagecount=0
+    attr_accessor :title, :author, :page_count, :genre
+
+
+
+    def initialize(title)
+        @title=title
+        
+    end
+
+    
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+        pagecount =+ 1
+    end
+
+end
+
 
 
